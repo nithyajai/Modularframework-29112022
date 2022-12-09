886a917677acd59ea23bc663c7fd6a0e1fae9a04
nithyajai <nithyajai23@gmail.com>
2022-12-09 20:13:21 +0100

    Update TestExcelDriverUtil.java

--- Modularframework-29112022/src/test/java/test/TestExcelDriverUtil.java
+++ Modularframework-29112022/src/test/java/test/TestExcelDriverUtil.java
@@ -1,1 +1,33 @@
-Hello
+package test;
+
+import org.testng.annotations.Test;
+
+import commonlibs.utils.Exceldriver;
+
+public class TestExcelDriverUtil {
+@Test
+public void VerifyTestExcelDriverUtil()throws Exception{
+	
+Exceldriver exceldriver=new Exceldriver();
+
+String excelworkbook=System.getProperty("user.dir")+"/testData/excelWorkbook.xlsx";
+
+String sheetname="TestData";
+
+exceldriver.createworkbook(excelworkbook);	
+
+exceldriver.openworkbook(excelworkbook);
+
+exceldriver.createsheet(sheetname);
+
+exceldriver.setcelldata(sheetname, 0, 0,"Nithya");
+exceldriver.setcelldata(sheetname, 0, 1,"jai");
+
+exceldriver.setcelldata(sheetname, 1, 0,"Dhana");
+exceldriver.setcelldata(sheetname, 1, 1,"NN");
+
+exceldriver.savefile();
+exceldriver.closeworkbook();
+}
+}
+
